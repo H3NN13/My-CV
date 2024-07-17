aee93eebc994b6d8c6e88bbf9bf33dcb5eee905a
H3NN13 <whitey.hendrik123@gmail.com>
2024-07-17 16:08:08 +0200

    Updated Animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component  } from '@angular/core';
+import { Router, RouterOutlet,NavigationEnd } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { ViewportScroller } from '@angular/common';
+ 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NavbarComponent],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-website';
+  constructor(private router: Router, private viewportScroller: ViewportScroller) { }
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.viewportScroller.scrollToPosition([0, 0]);
+      }
+    });
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-
+ 
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { routes } from '../app.routes';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+ 
+   
 }
