4d65a4e55a1853e8f785633cf14994173f4a1f2d
H3NN13 <whitey.hendrik123@gmail.com>
2024-07-18 10:57:15 +0200

    Added URL routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Router, RouterOutlet,NavigationEnd } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { ViewportScroller } from '@angular/common';
- 
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -14,7 +14,7 @@ import { ViewportScroller } from '@angular/common';
 export class AppComponent {
   title = 'my-website';
   constructor(private router: Router, private viewportScroller: ViewportScroller) { }
-
+// Lets user scroll automatically to top of page if new page is loaded
   ngOnInit() {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
