45b1425ef7f107d94afcb29109e4710034514986
H3NN13 <whitey.hendrik123@gmail.com>
2024-07-18 11:29:56 +0200

    Added Feature Click On Image

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { Router,RouterModule } from '@angular/router';
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [RouterModule ],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
 export class AboutMeComponent {
-
+  constructor(private router:Router) { }
+   routeTo(event: Event) { 
+    console.log(this.router);
+    // super cool function of angular: sets target to the event's target property and tells TS that it is an html element
+    const target = event.target as HTMLElement;
+    // sets route to the target element's ID
+    const route = target.id;
+      // Routes to the route provided
+      this.router.navigate([route]);
+     
+  }
 }
